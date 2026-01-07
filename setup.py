bdde6fdf0694592b82d5222d9d5a592dcdee507b
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2026-01-07 14:18:25 +0100

    place long running API calls in cysignals blocks (#384)
    * place long running API calls in cysignals blocks
    
    * add cysignals runtime dependency
    
    ---------
    
    Co-authored-by: Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>

--- setup.py
+++ setup.py
@@ -318,7 +318,8 @@ setup(
     cmdclass={"build_ext": my_build_ext},
     ext_modules=[make_extension()],
     test_suite="tests",
-    setup_requires=["Cython>=3.0.0,<3.2.0"],
+    setup_requires=["Cython>=3.0.0,<3.2.0", "cysignals"],
+    install_requires=["cysignals"],
     packages=["tesserocr"],
     package_dir={"tesserocr": "tesserocr"},
 )
